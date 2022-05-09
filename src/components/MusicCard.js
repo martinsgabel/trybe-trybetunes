8894a7aec28d3c95eacc644d9124d695d7deefb8
martins <gabrielamspinola@gmail.com>
2022-05-09 19:32:08 -0300

    Lint

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
 
 class MusicCard extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      checked: false,
+    };
+  }
+
+  isChecked(album) {
+    this.setState({
+      checked: true,
+    });
+
+    console.log(album);
+
+    addToFav(album);
+  }
+
+  async addToFav(album) {
+    const { checked } = this.state;
+    if (checked) {
+      await addSong(album);
+    }
+  }
+
   render() {
-    const { trackId, trackName, previewUrl } = this.props;
+    const { album: { trackId, trackName, previewUrl } } = this.props;
     return (
       <div>
         <p>{ trackName }</p>
@@ -15,15 +41,28 @@ class MusicCard extends React.Component {
             .
           </p>
         </audio>
+        <label
+          data-testid={ `checkbox-music-${trackId}` }
+          htmlFor="checkbox-id"
+        >
+          Favorita
+          <input
+            type="checkbox"
+            id="checkbox-id"
+            onClick={ () => this.isChecked(album) }
+          />
+        </label>
       </div>
     );
   }
 }
 
 MusicCard.propTypes = {
-  trackId: PropTypes.number.isRequired,
-  trackName: PropTypes.string.isRequired,
-  previewUrl: PropTypes.string.isRequired,
+  album: PropTypes.shape({
+    trackId: PropTypes.number.isRequired,
+    trackName: PropTypes.string.isRequired,
+    previewUrl: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default MusicCard;
